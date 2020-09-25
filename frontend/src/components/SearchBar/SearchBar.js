907858bf914caae8a1733965acaf1231ea5bc922
0haris0 <haris.valj@gmail.com>
2020-09-26 01:42:51 +0200

    Prepare SearchBar for backend

--- frontend/src/components/SearchBar/SearchBar.js
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -2,11 +2,31 @@ import React, { useState } from 'react';
 
 function SearchBar() {
     const [synonym, setSynonym] = useState('');
+    const [isError, setIsError] = useState(true);
+
+    const handleChange = (handleEvent) => {
+        switch (handleEvent.target.id) {
+            case 'search-bar':
+                setSynonym(handleEvent.target.value);
+                break;
+            default:
+
+
+                break;
+        }
+        if (synonym.length <= 1) {
+            setIsError(true);
+        } else {
+            setIsError(false)
+        }
+    }
+
     const Search = () => {
-        alert(synonym)
+        console.log('Submit form!');
+
     }
     return (
-        <form id="search">
+        <form id="search" onSubmit={(e) => e.preventDefault()}>
             <div className="form-group">
                 <input
                     className="form-control d-inline col-lg-8 col-12 mx-auto d-block"
@@ -14,11 +34,12 @@ function SearchBar() {
                     type="text"
                     placeholder="Search for synonyms"
                     value={synonym}
-                    onChange={(e) => setSynonym(e.target.value)}
+                    onChange={(e) => handleChange(e)}
                 ></input>
                 <button
                     type="submit"
-                    className="btn btn-primary d-inline col-lg-8 col-12 mx-auto mt-1 d-block"
+                    className="btn btn-primary d-inline col-lg-8 col-12 mx-auto mt-2 d-block"
+                    disabled={isError}
                     onClick={Search}
                 >Search</button>
             </div>
